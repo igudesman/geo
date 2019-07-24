a9b96aeabce5f8c167a3b0caf42903423eed45c8
Daniel Igudesman <digudesm@gmail.com>
2019-07-24 23:56:51 +0300

    v0.1.3
    Rosreestr parsing has been added

--- store/rosreestr/urls.py
+++ store/rosreestr/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.search, name='search'),
+	path('', views.search, name='search'),
+	path('not_found/', views.not_found, name='not_found'),
+	path('object_info/', views.object_info, name='object_info'),
 ]
--- store/rosreestr/views.py
+++ store/rosreestr/views.py
@@ -1,4 +1,58 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+import requests
+from bs4 import BeautifulSoup
+
+def object_info(request):
+	context = {}
+	if request.method == "POST":
+		info = []
+		data = request.POST.get('search')
+		data = data.replace(':', '-')
+		data = parse(data)
+
+		if data == []:
+			return redirect('not_found')
+
+		for element in data:
+			if ('Ранее учтенные' in element)  or ('устаревшей' in element):
+				print(element)
+				continue
+			info.append(element)
+
+		context = {
+			'info': info
+		}
+	if context == {}:
+		return redirect('search')
+
+	return render(request, 'rosreestr/object_info.html', context)
+
 
 def search(request):
-	return render(request, 'rosreestr/search.html', {})
+	context = {}
+
+	return render(request, 'rosreestr/search.html', context)
+
+def not_found(request):
+	context = {}
+
+	return render(request, 'rosreestr/not_found.html', context)
+
+def parse(cadastral_number):
+	data = []
+	url = 'https://rosreestr.net/kadastr/' + str(cadastral_number)
+	page = requests.get(url)
+
+	soup = BeautifulSoup(page.text, 'html.parser')
+
+	try:
+		if 'Страница не найдена' in soup.find_all('div', class_='order3')[0].find_all('h2')[0].get_text():
+			print('error')
+			return data
+	except Exception:
+		pass
+
+	for line in soup.find_all('div', class_='test__data')[0].find_all('div'):
+		data.append(line.get_text())
+
+	return data
